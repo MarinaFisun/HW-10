b1159f7d95cee146c950e71662dcbea640a3b121
Marina <m-fisun23@yandex.ru>
2023-08-07 12:26:02 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
-}
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+        public static void task1() {
+            System.out.println("Task 1");
+            int[] salaryArray = generateRandomArray();
+            int salarySum = 0;
+            for (int salary : salaryArray) {
+                salarySum = salarySum + salary;
+            }
+            System.out.println("Сумма трат за месяц составила " + salarySum + " рублей");
+        }
+    }
